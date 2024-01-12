7c03d91a0a757ceec65e1939cc100b360df73db8
repeckadonatas <repeckadonatas@yahoo.comm>
2024-01-12 20:14:18 +0200

    Wikimedia project start

---
+++ kafka-producer-wikimedia/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("java")
+}
+
+group = "io.conduktor.demos"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+    implementation("org.apache.kafka:kafka-clients:3.6.1")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.11")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    implementation("org.slf4j:slf4j-simple:2.0.11")
+
+    // Allows to read stream from Java code
+    // https://mvnrepository.com/artifact/com.squareup.okhttp3/okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    // Allows to grab a stream
+    // https://mvnrepository.com/artifact/com.launchdarkly/okhttp-eventsource
+    implementation("com.launchdarkly:okhttp-eventsource:4.1.1")
+
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
